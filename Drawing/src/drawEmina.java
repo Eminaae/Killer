3aa29c26be41875a147a4dd265cd3b70e4b931ae
Emina Arapcic <emina.arapcic@bitcamp.ba>
2015-06-09 16:21:07 +0200

    Task

--- Drawing/src/drawEmina.java
+++ Drawing/src/drawEmina.java
@@ -1,9 +1,37 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Arrays;
 
 public class drawEmina {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(Graphics g, String getln) {
+		g.setColor(Color.BLACK);
+		String partx;
+		String party;
+		String partwidth;
+		String parth;
 
+		String[] parts = getln.split(" ");
+
+		System.out.println(Arrays.toString(parts));
+		partx = parts[1];
+		party = parts[2];
+		partwidth = parts[3];
+		parth = parts[4];
+
+		int x = Integer.parseInt(partx);
+		int y = Integer.parseInt(party);
+		int width = Integer.parseInt(partwidth);
+		int h = Integer.parseInt(parth);
+
+		if (getln.contains("DO")) {
+			g.setColor(Color.BLACK);
+			g.drawOval(x, y, width, h);
+		}
+		if (getln.contains("DFO")) {
+			g.setColor(Color.ORANGE);
+			g.fillOval(x, y, width, h);
+		}
 	}
 
 }
